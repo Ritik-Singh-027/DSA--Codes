a967a68f40df0d1401f437f67f8dfbeb10626d97
Ritik-Singh-027 <assaulter027@gmail.com>
2025-10-15 15:08:20 +0530

    Added Search Insert Position  Solution

--- Binary Search/BS 1D Arrays/Search Insert Position.cpp
+++ Binary Search/BS 1D Arrays/Search Insert Position.cpp
@@ -31,5 +31,25 @@ public:
 
         // When not found, 'low' gives the correct insert position
         return low;
+
+        //using lower bound solution
+         /*
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = n; // default answer if all elements < target
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= target) {
+                ans = mid;      // possible answer, move left
+                high = mid - 1;
+            } else {
+                low = mid + 1;  // move right
+            }
+        }
+
+        return ans;
+        */
     }
 };
